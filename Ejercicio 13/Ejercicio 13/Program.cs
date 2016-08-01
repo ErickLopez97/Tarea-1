598293e637e180f690510f6b8be02a25788e6fb6
ErickLopez97 <erick029714@gmail.com>
2016-07-31 21:42:34 -0600

    Add files via upload

---
+++ Ejercicio 13/Ejercicio 13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v_num, v_num1;
+
+            Console.WriteLine("Ingrese un numero");
+            v_num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (v_num1 = 1; v_num1 <= 3; v_num1++)
+            {
+                Console.Write(+v_num);
+            }
+            Console.WriteLine();
+            for (v_num1 = 1; v_num1 <= 2; v_num1++)
+            {
+                Console.Write(+v_num + " ");
+            }
+            Console.WriteLine();
+            for (v_num1 = 1; v_num1 <= 2; v_num1++)
+            {
+                Console.Write(+v_num + " ");
+            }
+            Console.WriteLine();
+            for (v_num1 = 1; v_num1 <= 2; v_num1++)
+            {
+                Console.Write(+v_num + " ");
+            }
+            Console.WriteLine();
+            for (v_num1 = 1; v_num1 <= 3; v_num1++)
+            {
+                Console.Write(+v_num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio 14/Ejercicio 14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius, kelvin, farenheit;
+
+            Console.WriteLine("Ingrese los grados CELSIUS");
+            celsius = Convert.ToInt32(Console.ReadLine());
+            kelvin = celsius + 273;
+            Console.WriteLine("Grados CELSIUS a KELVIN son:      " + kelvin);
+            farenheit = (9 * celsius) / 5 + 32;
+            Console.WriteLine("Grados CELSIUS a FARENHEIT son:   " + farenheit);
+
+            Console.ReadKey();
+
+        }
+    }
+}
